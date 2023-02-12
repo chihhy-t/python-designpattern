b099c25d1e5d8fb7d81fa37fdc92d1b392b06d78
Chihiro Takai <83741708+ppt-chihirotakai@users.noreply.github.com>
2023-02-12 14:22:32 +0900

    fix strategy-pattern

--- strategy-pattern/main.py
+++ strategy-pattern/main.py
@@ -1,29 +1,35 @@
 from abc import ABCMeta, abstractmethod
 
-# Interface
-class Shape(metaclass=ABCMeta):
-  @abstractmethod
-  def area(self):
-    raise NotImplementedError()
+class Customer(metaclass=ABCMeta):
+	__normal_taxi_fare = 400
+	def __init__(self):
+		self.taxi_fare = self.__normal_taxi_fare
 
-class Rectangle(Shape):
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
-	def area(self) -> float:
-		return self.width * self.height
+	@abstractmethod
+	def getTaxiFare(self) -> int:
+		raise NotImplementedError
 
-class Circle(Shape):
-	def __init__(self, radius):
-		self.radius = radius
-	def area(self) -> float:
-		return self.radius * self.radius * 3.14
+class NormalCustomer(Customer):
+	def getTaxiFare(self) -> int:
+		return int(self.taxi_fare)
+
+class SeniorCustomer(Customer):
+	__senior_discount_rate = 10
+	def getTaxiFare(self) -> int:
+		return int(self.taxi_fare * (1 - (self.__senior_discount_rate / 100)))
+
+class FamiliyCustomer(Customer):
+	__family_discount_rate = 20
+	def getTaxiFare(self) -> int:
+		return int(self.taxi_fare * (1 - (self.__family_discount_rate / 100)))
 
 class main():
-  rectangle = Rectangle(10, 2)
-  print(rectangle.area())
-  circle = Circle(5)
-  print(circle.area())
+	normal_customer = NormalCustomer()
+	senior_customer = SeniorCustomer()
+	family_customer = FamiliyCustomer()
+	print(f"一般会員タクシー料金：¥{normal_customer.getTaxiFare()}")
+	print(f"シニア会員タクシー料金：¥{senior_customer.getTaxiFare()}")
+	print(f"家族会員タクシー料金：¥{family_customer.getTaxiFare()}")
 
 if __name__ == '__main__':
 	main()
